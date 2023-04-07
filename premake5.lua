c280189061fd09496b75ad30fe19983b18976e30
Fantasya63 Student <baloaloajansen.school@gmail.com>
2023-04-07 11:20:23 +0800

    Added glm

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,13 @@
- workspace "Candle"
-	architecture "x64"
-	startproject "Sandbox"
-	
-	configurations
-	{
-		"Debug",
-		"Release",
-		"Dist"
-	}
+workspace "Candle"
+architecture "x64"
+startproject "Sandbox"
+
+configurations
+{
+	"Debug",
+	"Release",
+	"Dist"
+}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
@@ -16,6 +16,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Candle/vendor/GLFW/include"
 IncludeDir["Glad"] = "Candle/vendor/Glad/include"
 IncludeDir["imgui"] = "Candle/vendor/imgui"
+IncludeDir["glm"] = "Candle/vendor/glm"
 
 include "Candle/vendor/GLFW"
 include "Candle/vendor/Glad"
@@ -23,120 +24,124 @@ include "Candle/vendor/imgui"
 
 
 project "Candle"
-	location "Candle"
-	kind "SharedLib"
-	language "C++"
-	staticruntime "off"
+location "Candle"
+kind "SharedLib"
+language "C++"
+staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	pchheader "cdpch.h"
-	pchsource "Candle/src/cdpch.cpp"
+pchheader "cdpch.h"
+pchsource "Candle/src/cdpch.cpp"
 
-	files
+files
+{
+	"%{prj.name}/src/**.h",
+	"%{prj.name}/src/**.cpp",
+	"%{prj.name}/vendor/glm/**.hpp",
+	"%{prj.name}/vendor/glm/**.inl",
+}
+
+includedirs
+{
+	"%{prj.name}/src",
+	"%{prj.name}/vendor/spdlog/include",
+	"%{IncludeDir.GLFW}",
+	"%{IncludeDir.Glad}",
+	"%{IncludeDir.imgui}",
+	"%{IncludeDir.glm}",
+}
+
+links
+{
+	"GLFW",
+	"Glad",
+	"imgui",
+	"opengl32.lib",
+}
+
+filter "system:windows"
+	cppdialect "C++17"
+	systemversion "latest"
+
+	defines
 	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
+		"CD_PLATFORM_WINDOWS",
+		"CD_BUILD_DLL",
+		"GLFW_INCLUDE_NONE",
 	}
 
-	includedirs
+	postbuildcommands
 	{
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.imgui}",
+		"{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox"
 	}
 
-	links
-	{
-		"GLFW",
-		"Glad",
-		"imgui",
-		"opengl32.lib",
-	}
-
-	filter "system:windows"
-		cppdialect "C++17"
-		systemversion "latest"
-
-		defines
-		{
-			"CD_PLATFORM_WINDOWS",
-			"CD_BUILD_DLL",
-			"GLFW_INCLUDE_NONE",
-		}
-
-		postbuildcommands
-		{
-			"{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox"
-		}
-
-	filter "configurations:Debug"
-		defines "CD_DEBUG"
-		symbols "On"
-		runtime "Debug"
+filter "configurations:Debug"
+	defines "CD_DEBUG"
+	symbols "On"
+	runtime "Debug"
 
 
-	filter "configurations:Release"
-		defines "CD_Release"
-		optimize "On"
-		runtime "Release"
+filter "configurations:Release"
+	defines "CD_Release"
+	optimize "On"
+	runtime "Release"
 
 
-	filter "configurations:Dist"
-		defines "CD_DIST"
-		symbols "On"
-		runtime "Release"
+filter "configurations:Dist"
+	defines "CD_DIST"
+	symbols "On"
+	runtime "Release"
 
 
 
 project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	language "C++"
+location "Sandbox"
+kind "ConsoleApp"
+language "C++"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
+files
+{
+	"%{prj.name}/src/**.h",
+	"%{prj.name}/src/**.cpp",
+}
+
+includedirs
+{
+	"Candle/vendor/spdlog/include",
+	"Candle/src",
+	"%{IncludeDir.glm}",
+}
+
+links
+{
+	"Candle"
+}
+
+filter "system:windows"
+	cppdialect "C++17"
+	staticruntime "On"
+	systemversion "latest"
+
+	defines
 	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
+		"CD_PLATFORM_WINDOWS",
 	}
 
-	includedirs
-	{
-		"Candle/vendor/spdlog/include",
-		"Candle/src"
-	}
-
-	links
-	{
-		"Candle"
-	}
-
-	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-		{
-			"CD_PLATFORM_WINDOWS",
-		}
-
-	filter "configurations:Debug"
-		defines "CD_DEBUG"
-		symbols "On"
+filter "configurations:Debug"
+	defines "CD_DEBUG"
+	symbols "On"
 
 
-	filter "configurations:Release"
-		defines "CD_Release"
-		optimize "On"
+filter "configurations:Release"
+	defines "CD_Release"
+	optimize "On"
 
 
-	filter "configurations:Dist"
-		defines "CD_DIST"
-		symbols "On"
+filter "configurations:Dist"
+	defines "CD_DIST"
+	symbols "On"
